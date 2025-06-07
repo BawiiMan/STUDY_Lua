383a40bf585f871c54254e5511473b8ca26638e3
bawiiman <rockyman0504@naver.com>
2025-06-08 08:22:00 +0900

    [FEAT] 익명 함수
    익명 함수는 테이블에 키 값으로 사용할 수 있다.

--- Lua_Lecture/000/ex07.lua
+++ Lua_Lecture/000/ex07.lua
@@ -1,4 +1,9 @@
 -- local function 함수명(인수들) 실행문들 end
 -- return : 함수의 실행을 즉시 중단, 반환값을 돌려준다
 -- 반환값이 두 개 이상일 수 있다.
+-- 함수의 인수가 단 하나이고 그것이 table 혹은 문자열일 경우
+-- 호출하는 쪽에서 괄호를 생략할 수 있다.
+-- 익명함수: function(인수들) 실행문들 end
 
+foo = function(a,b) return a+b end,
+goo = function(a,b) return a^b end
