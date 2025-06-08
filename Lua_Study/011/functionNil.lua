e0d8f7bc043ba21db8b1c69f10e8fc89fc1385ff
bawiiman <rockyman0504@naver.com>
2025-06-08 21:15:04 +0900

    [fix] 오류 수정

--- Lua_Study/011/functionNil.lua
+++ Lua_Study/011/functionNil.lua
@@ -1,8 +1,8 @@
-function f (a, b) print(A, b) end
+function f (a, b) print(a, b) end
 
 f(3)    --3 nil
 f(3, 4) -- 3 4
-f(3, 4, 5) -- 3 4 (5는 버려짐)
+--f(3, 4, 5) -- 3 4 (5는 버려짐)
 
 count = 0
 function incCount(n)
